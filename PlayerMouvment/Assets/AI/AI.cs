505920b25c5f1b484fbbd89bfdc38ecfe4f3a7c8
pow752 <charlied06@live.com>
2017-02-23 10:07:00 -0400

    update5
    cats2

---
+++ PlayerMouvment/Assets/AI/AI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour {
+
+    public float speed;
+    public Transform player;
+
+    static public GameObject other;
+    public Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+
+
+    void FixedUpdate()
+    {
+        float z = Mathf.Atan2((player.transform.position.y - transform.position.y), (player.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+
+        transform.eulerAngles = new Vector3(0, 0, z);
+        rb.AddForce(gameObject.transform.up * speed);
+
+    }
+
+	
+}
--- PlayerMouvment/Assets/Player/Player.cs
+++ PlayerMouvment/Assets/Player/Player.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+
+    public float HP = 100;
     public float speed;
     // public Rigidbody2D rb;
-   static public GameObject other;
+    static public GameObject other;
     public Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
-
 
 
 
@@ -18,6 +19,7 @@ public class Player : MonoBehaviour {
         rb.freezeRotation = true;
 
     }
+
 
     // Update is called once per frame
     void Update () {
@@ -29,24 +31,39 @@ public class Player : MonoBehaviour {
            rb.AddForce(Vector2.up * speed);
             // transform.position += Vector3.right * speed;
         }
-
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.AddForce(Vector2.left * speed);
             // transform.position += Vector3.right * speed;
         }
-
         if (Input.GetKey(KeyCode.DownArrow))
         {
             rb.AddForce(Vector2.down * speed);
             // transform.position += Vector3.right * speed;
         }
-
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.AddForce(Vector2.right * speed);
             // transform.position += Vector3.right * speed;
         }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            HP -= 1;
+            //rb.AddForce(Vector2.right * speed);
+            // transform.position += Vector3.right * speed;
+        }
+
+
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+
+
+        }
+
+
+
 
     }
 }
