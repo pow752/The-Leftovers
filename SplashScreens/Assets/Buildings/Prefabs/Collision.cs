6aa4cbd2aa76f845f4ffeae18eed2a1378c114a5
Cyros-Lugoth <Cyros_Lugoth@hotmail.com>
2017-03-27 19:56:31 -0300

    Buildings Added
    Skyscraper, Hospital, Fire Station, Barracks, Helipad, Warehouse, Weeb
    Shop, and Convenience Store are currently assets in the game.

---
+++ SplashScreens/Assets/Buildings/Prefabs/Collision.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collision : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public GameObject debris;
+    public Image hitPointBar;
+    public float maxHitPoints = 1000;
+    public float buildingHitPoints = 1000;
+    public float displayHitPoints = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+
+    void Destruction()
+    {
+        GameObject explode = Instantiate(explosionPrefab, new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z), Quaternion.identity) as GameObject;
+        GameObject makeDebris = Instantiate(debris, new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z), Quaternion.identity) as GameObject;
+        DestroyImmediate(hitPointBar, true);
+        Destroy(gameObject);
+    }
+
+    void Controls ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            buildingHitPoints -= 100;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Controls();
+        displayHitPoints = buildingHitPoints / maxHitPoints;
+        //hitPointBar.fillAmount = Mathf.Lerp(hitPointBar.fillAmount, displayHitPoints, Time.deltaTime * 2);
+        //hitPointBar.transform.localScale = new Vector3(displayHitPoints, hitPointBar.transform.localScale.y, hitPointBar.transform.localScale.z);
+        if (buildingHitPoints <= 0) Destruction();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "")   // If the building collides with the player.
+        {
+            buildingHitPoints -= 100;
+        }
+        
+    }
+}
---
+++ SplashScreens/Assets/Buildings/Prefabs/Explosion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer < 0.5f)
+        {
+            transform.localScale += new Vector3(0.03f, 0.03f, 0);
+        }
+        else
+        {
+            transform.localScale -= new Vector3(0.03f, 0.03f, 0);
+            if (timer > 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
