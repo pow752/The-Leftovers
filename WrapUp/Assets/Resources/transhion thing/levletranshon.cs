1381cdc7a4328d7af9cfc58508643983e3b20426
pow752 <charlied06@live.com>
2017-04-05 19:17:22 -0300

    levle tranze
    eragrhajnatr

---
+++ WrapUp/Assets/Resources/transhion thing/levletranshon.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levletranshon : MonoBehaviour {
+    public float UpdateTime;
+    public GameObject fade;
+    public int swich = 0;
+    public int nextlv = 0;
+
+
+    public float alfa =1;
+    private float UpdateTimer;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	void Update () {
+
+
+        if (swich==1)
+        {
+            UpdateTimer -= Time.deltaTime;
+
+            if (UpdateTimer < 0)
+            {
+                alfa -= .010f;
+               fade.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, alfa);
+                UpdateTimer += UpdateTime;
+
+                //  SpriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+            }
+
+            if (alfa<=-0.001)
+            {
+                swich = 0;
+
+            }
+        }
+
+
+
+        if (swich == 2)
+        {
+            UpdateTimer -= Time.deltaTime;
+
+            if (UpdateTimer < 0)
+            {
+                alfa += .010f;
+                fade.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, alfa);
+                UpdateTimer += UpdateTime;
+
+                //  SpriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+            }
+
+            if (alfa >= 1.001)
+            {
+                swich = 0;
+                Application.LoadLevel(nextlv);
+            }
+        }
+
+    }
+}
